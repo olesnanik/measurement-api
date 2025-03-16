73f6e4e2740d794d413942e01894b51d77be00e4
Marek Olešnaník <marek.olesnanik@justeattakeaway.com>
2025-03-16 21:54:33 +0100

    fix Swagger DTO schemas

--- src/modules/user/controllers/auth.controller.ts
+++ src/modules/user/controllers/auth.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { LoginUserDto } from '../dto/login-user-dto';
 import { LoginUserResponseDto } from '../dto/login-user-response-dto';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('user/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOkResponse({ type: LoginUserResponseDto })
   async login(
     @Body() loginUserDto: LoginUserDto,
   ): Promise<LoginUserResponseDto> {
